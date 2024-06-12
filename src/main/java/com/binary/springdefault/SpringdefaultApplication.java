fc386e6cc10603b00938b205ee3967bb308d156c
lorbusic <lorenzobusicchia87@gmail.com>
2024-06-12 19:38:14 +0200

    first commit

---
+++ src/main/java/com/binary/springdefault/SpringdefaultApplication.java
@@ -0,0 +1,13 @@
+package com.binary.springdefault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdefaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdefaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binary/springdefault/SpringdefaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binary.springdefault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdefaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
